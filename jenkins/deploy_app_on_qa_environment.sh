800ad1caeb005e6d571559a02256a050b03f0cbf
james-clarusway <jamesclarusway.com>
2022-10-03 07:55:24 +0000

    j-20

--- jenkins/deploy_app_on_qa_environment.sh
+++ jenkins/deploy_app_on_qa_environment.sh
@@ -5,8 +5,8 @@ AWS_REGION=$AWS_REGION helm repo add stable-petclinic s3://petclinic-helm-charts
 AWS_REGION=$AWS_REGION helm repo update
 helm package k8s/petclinic_chart
 AWS_REGION=$AWS_REGION helm s3 push --force petclinic_chart-${BUILD_NUMBER}.tgz stable-petclinic
-kubectl create ns petclinic-qa
-kubectl delete secret regcred -n petclinic-qa || true
+kubectl create ns petclinic-qa || echo "namespace petclinic-qa already exists"
+kubectl delete secret regcred -n petclinic-qa || echo "there is no regcred secret in petclinic-qa namespace"
 kubectl create secret generic regcred -n petclinic-qa \
     --from-file=.dockerconfigjson=/var/lib/jenkins/.docker/config.json \
     --type=kubernetes.io/dockerconfigjson
